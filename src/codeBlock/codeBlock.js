1caac962f6c074fc96ce4bb9da5e9c2eb26d51d3
particlec <74767651+particlec@users.noreply.github.com>
2021-06-03 17:21:27 +0800

    feat: 导航布局

---
+++ src/codeBlock/codeBlock.js
@@ -0,0 +1,33 @@
+import React from "react";
+import AceEditor from "react-ace";
+
+import "ace-builds/src-noconflict/mode-java";
+import "ace-builds/src-noconflict/theme-github";
+
+function CodeBlock() {
+    return(
+        <AceEditor
+            placeholder="Placeholder Text"
+            mode="javascript"
+            theme="monokai"
+            name="blah2"
+            // onLoad={this.onLoad}
+            // onChange={this.onChange}
+            fontSize={14}
+            showPrintMargin={true}
+            showGutter={true}
+            highlightActiveLine={true}
+            value={`function onLoad(editor) {
+  return xxx;
+}`}
+            setOptions={{
+                enableBasicAutocompletion: false,
+                enableLiveAutocompletion: false,
+                enableSnippets: false,
+                showLineNumbers: true,
+                tabSize: 2,
+            }}/>
+
+    )
+}
+export default CodeBlock;
---
+++ src/router/router.js
@@ -0,0 +1,33 @@
+import CodeBlock from '../codeBlock/codeBlock';
+import { HashRouter, Switch, Route, Redirect } from 'react-router-dom';
+import { Layout, Menu } from 'antd';
+
+const { Header, Content, Sider, Footer } = Layout;
+
+const AppRouter = () => {
+  return (
+    <HashRouter pathname="/home">
+      <Layout>
+        <Header></Header>
+        <Sider>
+          <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
+            <Menu.Item key="1">Option 1</Menu.Item>
+          </Menu>
+        </Sider>
+
+        <Content>
+          <Switch>
+            <Route path="/codeBlock">
+              <CodeBlock />
+            </Route>
+            <Redirect to="/codeBlock" />
+          </Switch>
+        </Content>
+        <Footer style={{ textAlign: 'center' }}>
+          Ant Design ©2018 Created by Ant UED
+        </Footer>
+      </Layout>
+    </HashRouter>
+  );
+};
+export default AppRouter;
