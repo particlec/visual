79ea2ecee3a90db276e47a29fbe487262f5aff22
particlec <74767651+particlec@users.noreply.github.com>
2021-06-03 18:02:23 +0800

    no message

--- src/router/router.js
+++ src/router/router.js
@@ -1,3 +1,4 @@
+import App from '../App';
 import CodeBlock from '../codeBlock/codeBlock';
 import { HashRouter, Switch, Route, Redirect } from 'react-router-dom';
 import { Layout, Menu } from 'antd';
@@ -6,59 +7,40 @@ const { Header, Content, Sider, Footer } = Layout;
 
 const AppRouter = () => {
   return (
-
-      <HashRouter>
-        <Switch>
-
-          <LayoutRoute
-              component={CodeBlock}
-              layout={SidebarLayout}
-              path="/manager/word_rule_config"
-          />
-
-
-          <LayoutRoute
-              component={ChineseSentenceRuleConfigDetail}
-              layout={SidebarLayout}
-              path="/manager/cn_sentence_rule_config/detail"
-          />
-        </Switch>
-      </HashRouter>
-
-
-
-
-
-
-
-
-
-
-
-
-
     <HashRouter pathname="/home">
-      <Layout>
-        <Header></Header>
-        <Sider>
-          <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
-            <Menu.Item key="1">Option 1</Menu.Item>
-          </Menu>
-        </Sider>
+      <Switch>
+        <LayoutRoute
+          component={CodeBlock}
+          layout={SidebarLayout}
+          path="/codeBlock"
+        />
 
-        <Content>
-          <Switch>
-            <Route path="/codeBlock">
-              <CodeBlock />
-            </Route>
-            <Redirect to="/codeBlock" />
-          </Switch>
-        </Content>
-        <Footer style={{ textAlign: 'center' }}>
-          Ant Design ©2018 Created by Ant UED
-        </Footer>
-      </Layout>
+        <LayoutRoute component={App} layout={SidebarLayout} path="/app" />
+      </Switch>
     </HashRouter>
+
+    // <HashRouter pathname="/home">
+    //   <Layout>
+    //     <Header></Header>
+    //     <Sider>
+    //       <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
+    //         <Menu.Item key="1">Option 1</Menu.Item>
+    //       </Menu>
+    //     </Sider>
+    //
+    //     <Content>
+    //       <Switch>
+    //         <Route path="/codeBlock">
+    //           <CodeBlock />
+    //         </Route>
+    //         <Redirect to="/codeBlock" />
+    //       </Switch>
+    //     </Content>
+    //     <Footer style={{ textAlign: 'center' }}>
+    //       Ant Design ©2018 Created by Ant UED
+    //     </Footer>
+    //   </Layout>
+    // </HashRouter>
   );
 };
 export default AppRouter;
